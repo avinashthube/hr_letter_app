c8450edf69013877eaf782b9c7fad76461915f5a
avinashthube <avinash.thube@successive.tech>
2019-08-12 12:21:59 +0530

    fix:directory structure modified

--- hrletter-app/src/Controllers/database/database.module.ts
+++
@@ -1,8 +0,0 @@
-import { databaseProviders } from './databaseProviders';
-import { Module } from '@nestjs/common';
-
-@Module({
-  providers: [...databaseProviders],
-  exports: [...databaseProviders],
-})
-export class DatabaseModule {}
