9a5e103ca05395a0e830ebaec1095f69a1662c71
avinashthube <avinash.thube@successive.tech>
2019-08-12 19:51:12 +0530

    add: user controller and userService CRUD operations added

--- hrletter-app/src/controller/user.controller.ts
+++ hrletter-app/src/controller/user.controller.ts
@@ -1,6 +1,36 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param, Delete, Put, Body } from '@nestjs/common';
 import { UserService } from 'src/service/user.service';
+import { UserType } from 'src/dto/user.dto';
 @Controller('employee/profile')
 export class UserController {
   constructor(private readonly userService: UserService) {}
+
+  @Get(':userID')
+  async findSingleUser(@Param('userID') userID: string) {
+    const result = this.userService.findSingleUser(userID);
+    console.log(result);
+    return result;
+  }
+
+  @Get()
+  async findAllUsers() {
+    const result = await this.userService.findAllUsers();
+    console.log(result);
+    return result;
+  }
+  /*
+  //delete not required
+  @Delete(':userID')
+  async deleteUser(@Param('userID') userID: string) {
+    return await this.userService(userID);
+  }*/
+
+  @Put(':userId')
+  async updateUser(
+    @Param('userId') userId: string,
+    @Body() createUserDto: UserType,
+  ) {
+    return await this.userService.updateUser(userId, createUserDto);
+    // return 'Record Updated';
+  }
 }
--- hrletter-app/src/service/user.service.ts
+++ hrletter-app/src/service/user.service.ts
@@ -1,6 +1,6 @@
 import { UserInterface } from './../interface/user.interface';
 import { Model } from 'mongoose';
-import { Injectable, Inject } from '@nestjs/common';
+import { Injectable, Inject, NotFoundException } from '@nestjs/common';
 import { UserInput } from 'src/graphql/input/user.input';
 @Injectable()
 export class UserService {
@@ -20,5 +20,41 @@ export class UserService {
       console.log('User record creation failure');
       return 'User record creation failure';
     }
+  }
+
+  async findSingleUser(userID: string) {
+    let user;
+    try {
+      user = await this.userModel.find({ userID: userID }).exec();
+    } catch (error) {
+      throw new NotFoundException('Could not find  user.');
+    }
+    if (!user) {
+      throw new NotFoundException('Could not find  user.');
+    }
+    console.log(user);
+    return user;
+  }
+
+  async findAllUsers() {
+    return await this.userModel.find().exec();
+  }
+
+  async updateUser(userId: string, createUserDto: UserInput) {
+    return await this.userModel
+      .findOneAndUpdate(
+        { userID: userId },
+        {
+          firstName: createUserDto.firstName,
+          lastName: createUserDto.lastName,
+          email: createUserDto.email,
+          updatedBy: createUserDto.updatedBy,
+          lastUpdateDate: Date.now(),
+        },
+        {
+          new: true,
+        },
+      )
+      .exec();
   }
 }
